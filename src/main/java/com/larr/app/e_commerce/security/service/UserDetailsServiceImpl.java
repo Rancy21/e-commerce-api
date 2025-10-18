e79073f1724b87efc2aa33f2f7107f098284ec21
Rancy Onstandaga <rancyontsandaga@gmail.com>
2025-10-18 19:44:11 +0200

    Added service annotation for bean injection

--- src/main/java/com/larr/app/e_commerce/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/larr/app/e_commerce/security/service/UserDetailsServiceImpl.java
@@ -8,10 +8,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.larr.app.e_commerce.model.User;
 import com.larr.app.e_commerce.repository.UserRepository;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
