5f0c24df06f9ed253671747edb73b7d3ba51be3e
Rancy Onstandaga <rancyontsandaga@gmail.com>
2025-10-18 19:43:48 +0200

    fixed Jpa extension

--- src/main/java/com/larr/app/e_commerce/repository/CartItemRepository.java
+++ src/main/java/com/larr/app/e_commerce/repository/CartItemRepository.java
@@ -3,7 +3,9 @@ package com.larr.app.e_commerce.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.larr.app.e_commerce.model.CartItem;
+
 @Repository
-public interface CartItemRepository extends JpaRepository<CartItemRepository, String> {
+public interface CartItemRepository extends JpaRepository<CartItem, String> {
 
 }
