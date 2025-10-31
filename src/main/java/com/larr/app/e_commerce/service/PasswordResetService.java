8803615a3f37f0dee275d8f3c4bd3f5c071a7635
Rancy Onstandaga <rancyontsandaga@gmail.com>
2025-10-31 15:52:49 +0200

    Added more comments

--- src/main/java/com/larr/app/e_commerce/service/PasswordResetService.java
+++ src/main/java/com/larr/app/e_commerce/service/PasswordResetService.java
@@ -15,6 +15,15 @@ import com.larr.app.e_commerce.model.User;
 import com.larr.app.e_commerce.repository.PasswordResetRepository;
 import com.larr.app.e_commerce.repository.UserRepository;
 
+/**
+ * Service class for handling password reset functionality.
+ * 
+ * Responsibilities:
+ * 1. Generate password reset tokens
+ * 2. Send reset emails to users
+ * 3. Validate reset tokens
+ * 4. Update user passwords
+ */
 @Service
 public class PasswordResetService {
     @Autowired
@@ -50,19 +59,24 @@ public class PasswordResetService {
         sendResetEmail(email, token);
     }
 
+    // Send password reset email to user
     public void sendResetEmail(String email, String token) {
+        // build the reset link
         String resetLink = "http://localhost:8080/reset-password.html?token=" + token; // reset-password.html must be
                                                                                        // created for this link to work
 
+        // Create email message
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(email);
         message.setSubject("Password Reset Request");
         message.setText("Clik the link to reset your password: " + resetLink
                 + "\n\nThis link will expire in 24 hours");
 
+        // send the email
         mailSender.send(message);
     }
 
+    // Validate if a reset token is valid and not expired
     public boolean validateToken(String token) {
         Optional<PasswordResetToken> resetToken = tokenRepository.findByToken(token);
 
@@ -73,17 +87,24 @@ public class PasswordResetService {
         return resetToken.get().getExpiryDate().isAfter(LocalDateTime.now());
     }
 
+    // Reset user's password using a valid token.
     public void resetPassword(String token, String newPassword) {
+        // find the token in the database
         PasswordResetToken resetToken = tokenRepository.findByToken(token)
                 .orElseThrow(() -> new RuntimeException("Invalid Token"));
 
+        // Check if the token has expired
         if (resetToken.getExpiryDate().isBefore(LocalDateTime.now())) {
             throw new RuntimeException("Token Expired");
         }
 
+        // Get the user associated with this token
         User user = resetToken.getUser();
 
+        // Hash the new password
         user.setPassword(passwordEncoder.encode(newPassword));
+
+        // Save the updated user in the database
         uRepository.save(user);
 
         // Delete used token
