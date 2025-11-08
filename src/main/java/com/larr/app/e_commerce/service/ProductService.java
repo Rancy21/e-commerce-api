b639691739bd3d92f734a6508d3ed62314e4367c
Rancy21 <rancyontsandaga@gmail.om>
2025-11-08 14:13:01 +0200

    feat: implement ProductService with CRUD operations for products

---
+++ src/main/java/com/larr/app/e_commerce/service/ProductService.java
@@ -0,0 +1,88 @@
+package com.larr.app.e_commerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.larr.app.e_commerce.model.Category;
+import com.larr.app.e_commerce.model.Product;
+import com.larr.app.e_commerce.repository.ProductRepository;
+
+@Service
+public class ProductService {
+  private ProductRepository repo;
+
+  @Autowired
+  public ProductService(ProductRepository repository) {
+    this.repo = repository;
+  }
+
+  public Product createProduct(Product product) {
+    return repo.save(product);
+  }
+
+  public Product findProduct(String id) {
+    Optional<Product> existingProduct = repo.findById(id);
+    if (existingProduct.isPresent()) {
+      return existingProduct.get();
+    } else {
+      return null;
+    }
+  }
+
+  public Product increaseProductQuantity(Product product, int quantity) {
+    int newQuantity = product.getQuantity() + quantity;
+
+    product.setQuantity(newQuantity);
+
+    return repo.save(product);
+  }
+
+  public Product decreaseProductQuantity(Product product, int quantity) {
+    int newQuantity = product.getQuantity() - quantity;
+
+    product.setQuantity(newQuantity);
+
+    return repo.save(product);
+  }
+
+  public Product updateProductName(Product product, String name) {
+    product.setName(name);
+    return repo.save(product);
+  }
+
+  public Product updateProductCategory(Product product, Category category) {
+    product.setCategory(category);
+    return repo.save(product);
+  }
+
+  public Product deleteProduct(Product product) {
+    product.setActive(false);
+    return repo.save(product);
+  }
+
+  public Product updateProductDescription(Product product, String description) {
+    product.setDescription(description);
+    return repo.save(product);
+  }
+
+  public Product updateProductPrice(Product product, double price) {
+    product.setPrice(price);
+    return repo.save(product);
+  }
+
+  public Product updateProductImageUrl(Product product, String url) {
+    product.setImgUrl(url);
+    return repo.save(product);
+  }
+
+  public List<Product> findProductsByCategory(Category category) {
+    return repo.findAllByCategory(category);
+  }
+
+  public List<Product> finProductsByName(String name) {
+    return repo.findByNameContainingIgnoreCase(name);
+  }
+}
