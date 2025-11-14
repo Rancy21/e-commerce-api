1e441d243b631e5de5e29a6fce1f840737bf207a
Rancy21 <rancyontsandaga@gmail.om>
2025-11-14 15:35:37 +0200

    update: added procuct retrieval with name

--- src/main/java/com/larr/app/e_commerce/repository/ProductRepository.java
+++ src/main/java/com/larr/app/e_commerce/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.larr.app.e_commerce.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.larr.app.e_commerce.model.Category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.larr.app.e_commerce.model.Product;
@@ -17,4 +20,7 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     List<Product> findAllByCategory(Category category);
 
     List<Product> findByNameContainingIgnoreCase(String name);
+
+    @Query("FROM Product p WHERE p.name = :name and p.isActive = true")
+    Optional<Product> findProductByName(@Param("name") String name);
 }
