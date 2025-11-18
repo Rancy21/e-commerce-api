f8199df4b2c5af73353c2555416d851e3eb40249
Rancy21 <rancyontsandaga@gmail.om>
2025-11-18 11:01:06 +0200

    feat: add methods for finding and deleting CartItems by Cart

--- src/main/java/com/larr/app/e_commerce/repository/CartItemRepository.java
+++ src/main/java/com/larr/app/e_commerce/repository/CartItemRepository.java
@@ -1,11 +1,19 @@
 package com.larr.app.e_commerce.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.larr.app.e_commerce.model.Cart;
 import com.larr.app.e_commerce.model.CartItem;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, String> {
+  List<CartItem> findByCart(Cart cart);
 
+  Optional<CartItem> findByIdAndCartId(String id, String cartId);
+
+  void deleteAllByCart(Cart cart);
 }
