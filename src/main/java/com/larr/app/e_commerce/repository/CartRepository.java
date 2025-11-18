5c1f53356d70703bfd13a76a9afb226331f678a3
Rancy21 <rancyontsandaga@gmail.om>
2025-11-18 13:09:58 +0200

    feat: add method to find Cart by user ID and status in CartRepository

--- src/main/java/com/larr/app/e_commerce/repository/CartRepository.java
+++ src/main/java/com/larr/app/e_commerce/repository/CartRepository.java
@@ -1,13 +1,17 @@
 package com.larr.app.e_commerce.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.larr.app.e_commerce.model.Cart;
+import com.larr.app.e_commerce.model.CartStatus;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, String> {
     List<Cart> findAll();
+
+    Optional<Cart> findCartByUserIdAndStatus(String userId, CartStatus status);
 }
