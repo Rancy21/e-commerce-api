023f400ec86bce6178e4ccab00937c436d310b74
Rancy21 <rancyontsandaga@gmail.om>
2025-11-19 16:59:16 +0200

    refactor: replace deleteAllByCart method with deleteByCart for improved clarity

--- src/main/java/com/larr/app/e_commerce/repository/CartItemRepository.java
+++ src/main/java/com/larr/app/e_commerce/repository/CartItemRepository.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.larr.app.e_commerce.model.Cart;
@@ -15,5 +18,7 @@ public interface CartItemRepository extends JpaRepository<CartItem, String> {
 
   Optional<CartItem> findByIdAndCartId(String id, String cartId);
 
-  void deleteAllByCart(Cart cart);
+  @Modifying
+  @Query("delete from CartItem c where c.cart.id = :id")
+  void deleteByCart(@Param("id") String cartId);
 }
