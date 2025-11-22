7fb136e843618023ea9e3320ed3c7e932f32af14
Rancy21 <rancyontsandaga@gmail.om>
2025-11-22 18:20:15 +0200

    refractor: change User relationship from 'one to one' to 'one to many'

--- src/main/java/com/larr/app/e_commerce/model/Payment.java
+++ src/main/java/com/larr/app/e_commerce/model/Payment.java
@@ -15,6 +15,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -27,7 +28,7 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "cart_id")
     private Cart cart;
     @OneToOne
